412c91087bd245f2494969327697201d0c8b4460
msafin <93736845@qq.com>
2013-02-04 16:42:34 +0800

    使用actionbarsherlock和viewpagerindicator完成框架搭建

--- android/src/com/sharegogo/video/MainActivity.java
+++ android/src/com/sharegogo/video/MainActivity.java
@@ -1,20 +1,79 @@
 package com.sharegogo.video;
 
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.MenuItem;
+import com.viewpagerindicator.IconPagerAdapter;
+import com.viewpagerindicator.TabPageIndicator;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
-    @Override
+public class MainActivity extends SherlockFragmentActivity {
+    private static final String[] CONTENT = new String[] { "Calendar", "Camera", "Alarms", "Location" };
+    private static final int[] ICONS = new int[] {
+            R.drawable.perm_group_calendar,
+            R.drawable.perm_group_camera,
+            R.drawable.perm_group_device_alarms,
+            R.drawable.perm_group_location,
+    };
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
         setContentView(R.layout.activity_main);
-    }
+        
+        FragmentPagerAdapter adapter = new GoogleMusicAdapter(getSupportFragmentManager());
 
+        ViewPager pager = (ViewPager)findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+
+        TabPageIndicator indicator = (TabPageIndicator)findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
+    }
+    
+	
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
+	public boolean onCreateOptionsMenu(com.actionbarsherlock.view.Menu menu) {
+		// TODO Auto-generated method stub
+        MenuItem populateItem = menu.add(Menu.NONE, 1, 0, "Populate");
+        populateItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        MenuItem clearItem = menu.add(Menu.NONE, 2, 0, "Clear");
+        clearItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM);
+        
+        return super.onCreateOptionsMenu(menu);
+	}
+
+
+	class GoogleMusicAdapter extends FragmentPagerAdapter implements IconPagerAdapter{
+        public GoogleMusicAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return TestFragment.newInstance(CONTENT[position % CONTENT.length]);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return CONTENT[position % CONTENT.length].toUpperCase();
+        }
+
+        @Override 
+        public int getIconResId(int index) {
+            return ICONS[index];
+          }
+        
+        @Override
+        public int getCount() {
+            return CONTENT.length;
+        }
     }
 }
+
+
